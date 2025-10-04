61f4447d0d19480e2f15db9b2e047bb88143a7f1
son9ih <son9ih@kaist.ac.kr>
2025-10-04 14:56:57 +0900

    update: path

--- synther/online/online_cond.py
+++ synther/online/online_cond.py
@@ -363,6 +363,8 @@ def get_time_limit(env: gym.Env):
 
 if __name__ == '__main__':
     import argparse
+    from datetime import datetime
+    import os
     
 
     parser = argparse.ArgumentParser()
@@ -379,6 +381,11 @@ if __name__ == '__main__':
     parser.add_argument('--synther', action='store_true', default=False)
     
     args = parser.parse_args()
+    
+    args.results_folder = f'./{args.results_folder}/{args.results_folder}_{args.env}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+    print(args.results_folder)
+    if not os.path.exists(args.results_folder):
+        os.makedirs(args.results_folder)
 
     logger_kwargs = setup_logger_kwargs(args.env, args.log_dir)
 
